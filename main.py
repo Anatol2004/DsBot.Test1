55e99bf0d13928de5914a8a3883a39650eb189a1
DE024 <deomanta123@gmail.com>
2025-02-13 21:08:08 +0300

    DsBotCommit.Test1(Ver 0.1)

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+import disnake
+from disnake.ext import commands
+
+bot = commands.Bot(command_prefix = "!pbot1", help_command = None, intents = disnake.Intents.all())
+
+
+@bot.event
+async def on_ready():
+    print(f"Bot {bot.user} is ready to work!")
+
+bot.run("MTMzNjI1MzcwOTY0MTg0Mjc2MA.G_OBDu.ebyHxyIXJ-bQgIeNBDvZ9TNzTEq2L0nPnXB2cE")
